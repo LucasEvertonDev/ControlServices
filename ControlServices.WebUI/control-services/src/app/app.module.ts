fa0543c50f299a974d18d38911dc88fe4b388204
Lcs Everton <lcseverton@outlook.com>
2023-08-01 11:32:45 -0300

    Alterações layout

--- ControlServices.WebUI/control-services/src/app/app.module.ts
+++ ControlServices.WebUI/control-services/src/app/app.module.ts
@@ -3,22 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LayoutNavBarComponent } from './shared/layout-nav-bar/layout-nav-bar.component';
-import { LayoutMenuComponent } from './shared/layout-menu/layout-menu.component';
-import { LayoutFooterComponent } from './shared/layout-footer/layout-footer.component';
+import { LayoutNavBarComponent } from './shared/theme/layout-nav-bar/layout-nav-bar.component';
+import { LayoutMenuComponent } from './shared/theme/layout-menu/layout-menu.component';
+import { LayoutFooterComponent } from './shared/theme/layout-footer/layout-footer.component';
+import { LayoutComponent } from './shared/theme/layout/layout.component';
+import { LayoutAccountComponent } from './shared/theme/layout-account/layout-account.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutNavBarComponent,
     LayoutMenuComponent,
-    LayoutFooterComponent
+    LayoutFooterComponent,
+    LayoutComponent,
+    LayoutAccountComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent, LayoutMenuComponent, LayoutNavBarComponent, LayoutFooterComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
