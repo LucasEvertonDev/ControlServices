69639dc1b0c4ffdca2edc70e418c5fb1671ef08f
Lcs Everton <lcseverton@outlook.com>
2023-08-01 14:43:07 -0300

    dark mode

--- ControlServices.WebUI/control-services/src/app/shared/theme/layout-menu/layout-menu.component.ts
+++ ControlServices.WebUI/control-services/src/app/shared/theme/layout-menu/layout-menu.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { SCRIPTS } from 'src/app/common/constants/scripts-constants';
+import { ScriptsService } from 'src/app/common/services/scripts/scripts.service';
 
 @Component({
   selector: 'app-layout-menu',
   templateUrl: './layout-menu.component.html',
   styleUrls: ['./layout-menu.component.scss']
 })
-export class LayoutMenuComponent {
+export class LayoutMenuComponent  implements OnInit, AfterViewInit{
 
+  public constructor(private _scripts:ScriptsService) {
+  }
+
+  public ngAfterViewInit(): void {
+    this._scripts.load(SCRIPTS.THEME_HELPERS, SCRIPTS.THEME_CONFIG, SCRIPTS.JQUERY, SCRIPTS.POPPER, SCRIPTS.BOOTSTRAP, SCRIPTS.PERFECT_SCROOL_BAR, SCRIPTS.THEME_MENU, SCRIPTS.THEME_MAIN, SCRIPTS.THEME_BUTTON);
+  }
+
+  public ngOnInit(): void {
+  }
 }
+
--- ControlServices.WebUI/control-services/src/app/shared/theme/layout/layout.component.ts
+++ ControlServices.WebUI/control-services/src/app/shared/theme/layout/layout.component.ts
@@ -1,21 +1,8 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { SCRIPTS } from 'src/app/common/constants/scripts-constants';
-import { ScriptsService } from 'src/app/common/services/scripts/scripts.service';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent implements OnInit, AfterViewInit{
-
-  public constructor(private _scripts:ScriptsService) {
-  }
-
-  public ngAfterViewInit(): void {
-    this._scripts.load(SCRIPTS.THEME_HELPERS, SCRIPTS.THEME_CONFIG, SCRIPTS.JQUERY, SCRIPTS.POPPER, SCRIPTS.BOOTSTRAP, SCRIPTS.PERFECT_SCROOL_BAR, SCRIPTS.THEME_MENU, SCRIPTS.THEME_MAIN, SCRIPTS.THEME_BUTTON);
-  }
-
-  public ngOnInit(): void {
-  }
-}
+export class LayoutComponent {}
